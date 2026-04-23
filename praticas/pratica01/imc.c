214a5e87818039def365ec258fab6ecd03f633a0
Sofia Dios <smaria.dios@gmail.com>
2026-04-23 17:25:41 -0300

    concertando erros

--- praticas/pratica01/imc.c
+++ praticas/pratica01/imc.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
+
 int main() {
     float peso, altura, imc;
 
-    printf("Digite seu peso (em kg): ");
+    printf("Digite o peso (kg): ");
     scanf("%f", &peso);
 
-    printf("Digite sua altura (em metros): ");
+    printf("Digite a altura (m): ");
     scanf("%f", &altura);
 
     imc = peso / (altura * altura);
 
-    printf("Seu IMC é: %.2f\n", imc);
+    printf("IMC: %.2f\n", imc);
 
     if (imc < 18.5) {
-        printf("Classificação: Abaixo do peso\n");
-    } else if (imc < 25.0) {
-        printf("Classificação: Peso normal\n");
-    } else if (imc  < 30.0) {
-        printf("Classificação: Sobrepeso\n");
-    } else if (imc < 35.0) {
-        printf("Classificação: Obesidade grau I\n");   
-    }  else if (imc < 40.0) {
-        printf("Classificação: Obesidade grau II\n");
+        printf("Situacao: abaixo do peso\n");
+    } else if (imc <= 24.9) {
+        printf("Situacao: peso normal\n");
+    } else if (imc <= 29.9) {
+        printf("Situacao: sobrepeso\n");
     } else {
-        printf("Classificação: Obesidade grau III\n");
+        printf("Situacao: obesidade\n");
     }
 
     return 0;
